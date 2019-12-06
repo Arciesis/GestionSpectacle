ce8d67900a9c932960b10be402eea6290920e810
mathis favier <favier.mathis@gmail.com>
2019-12-06 15:20:35 +0100

    Ajout de la classe Interpretes et des champs dans Spectacle

---
+++ src/Interpretes.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Interpretes {
+    private ArrayList<String> listInterpretes;
+
+}
--- src/Spectacle.java
+++ src/Spectacle.java
@@ -1,2 +1,6 @@
 public class Spectacle {
+    protected int idSpectacle;
+    protected String titre;
+    protected Interpretes lesInterpretes;
+
 }
