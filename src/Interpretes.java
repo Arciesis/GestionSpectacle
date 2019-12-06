8573a81dee0939ea781c4762b25da41178bc4567
mathis favier <favier.mathis@gmail.com>
2019-12-06 16:07:10 +0100

    ajout du champ et du constructeur de la classe Interpretes

--- src/Interpretes.java
+++ src/Interpretes.java
@@ -1,6 +1,19 @@
 import java.util.ArrayList;
 
 public class Interpretes {
+    /**
+     * Unique champ de la classe contenant les Interpretes
+     */
     private ArrayList<String> listInterpretes;
+
+    /**
+     * Constructeur de la classe
+     * Construit une liste vide
+     */
+    public Interpretes(){
+        this.listInterpretes = new ArrayList<>();
+    }
+
+
 
 }
