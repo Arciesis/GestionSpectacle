42638e127ba6ee119034042d3aef9cd27edc6147
mathis favier <favier.mathis@gmail.com>
2019-12-11 15:18:12 +0100

    AJout des getters de la Classe Creneau ainsi que les methodes toString et equals

--- src/Creneau.java
+++ src/Creneau.java
@@ -1,13 +1,7 @@
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Creneau {
-
-    private static int nextIdCreneau;
-
-    /**
-     * initialiser a 1 et en autoincrementation de 1
-     */
-    private int idCreneau;
 
     /**
      * jour de la semaine compris entre 1 et 7
@@ -25,10 +19,6 @@ public class Creneau {
      */
     private Horaire heureDebut;
 
-    static {nextIdCreneau=1;}
-    {idCreneau=nextIdCreneau; nextIdCreneau++;}
-
-
     /**
      * Constructeur de laq classe Creneau
      * @param jour le jour de la semaine compris entre 1 et 7
@@ -41,4 +31,48 @@ public class Creneau {
         this.heureDebut=heureDebut;
         this.heureFin=heureFin;
     }
+
+    /**
+     * getter du champ jour
+     * @return le jour du creneau
+     */
+    public int getJour() {
+        return jour;
+    }
+
+    /**
+     * getter du champ HeureFin
+     * @return l'oraire de fin du creneau
+     */
+    public Horaire getHeureFin() {
+        return heureFin;
+    }
+
+    /**
+     * getter du champ heureDebut
+     * @return l'Horaire de debut du creneau
+     */
+    public Horaire getHeureDebut() {
+        return heureDebut;
+    }
+
+    @Override
+    public String toString() {
+        return "Creneau{" +
+                "jour=" + jour +
+                ", heureFin=" + heureFin +
+                ", heureDebut=" + heureDebut +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Creneau creneau = (Creneau) o;
+        return jour == creneau.jour &&
+                heureFin.equals(creneau.heureFin) &&
+                heureDebut.equals(creneau.heureDebut);
+    }
+
 }
