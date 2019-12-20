0f49185540ecf39eb2dfba15576895fa9deaf88f
mathis favier <favier.mathis@gmail.com>
2019-12-20 16:49:17 +0100

    le toout fonctionnant

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String args[]){
+
         // write your code here
         Creneau c = new Creneau(1,new Horaire(11,30),new Horaire(13,30));
         System.out.println(c.calculerDuree());
@@ -21,6 +22,15 @@ public class Main {
         System.out.println(c.compareTo(new Creneau(1,new Horaire(12,30),new Horaire(14,30))));
 
         // System.out.println(s.toString());
+        s.ajouterCreneau(new Creneau(1,new Horaire(10,30),new Horaire(11,30)));
+        s.ajouterCreneau(new Creneau(1,new Horaire(13,30),new Horaire(15,30)));
+        s.ajouterCreneau(c);
+        s.ajouterCreneau(new Creneau(1,new Horaire(11,30),new Horaire(12,30)));
+        s.ajouterCreneau(new Creneau(1,new Horaire(10,30),new Horaire(12,30)));
+        s.ajouterCreneau(new Creneau(1,new Horaire(12,30),new Horaire(14,30)));
+
+
+        System.out.println(s.toString());
     }
 
 }
